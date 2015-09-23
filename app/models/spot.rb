7c3791818354a0f5ec44202bf03bb3bec8c406f4
Yusuke Naito <yusuke-naito@cookpad.com>
2015-09-23 16:08:31 +0900

    Add base setting

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,26 @@
 class Spot < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name "#{Rails.env}-#{Rails.application.class.to_s.downcase}-#{self.name.downcase}"
+
+  mapping do
+    indexes :id, type: 'string', index: 'not_analyzed'
+    indexes :spot_name, type: 'string', analyzer: 'kuromoji'
+    indexes :address, type: 'string', analyzer: 'kuromoji'
+    indexes :location, type: 'geo_point'
+  end
+
+  settings index: {
+    number_of_shards: 1,
+    number_of_replicas: 0,
+  }
+
+  def as_indexed_json(options = {})
+    { 'id'        => id,
+      'spot_name' => name,
+      'address'   => address,
+      'location'  => "#{lat},#{lon}",
+    }
+  end
 end
